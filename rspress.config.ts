bca5d84be3c1f76306f48506474314658a02394e
Zero <1300326388@qq.com>
2023-10-28 00:21:56 +0800

    更新 icon

--- rspress.config.ts
+++ rspress.config.ts
@@ -6,8 +6,8 @@ export default defineConfig({
   base: '/site/',
   title: 'FlutterAds',
   description: '让你的 Flutter 应用广告收益翻倍',
-  icon: "/flutterads_icon.png",
-  logo: "/flutterads_logo.png",
+  icon: "/icon.png",
+  logo: "/logo.png",
   themeConfig: {
     search: false,
     socialLinks: [
