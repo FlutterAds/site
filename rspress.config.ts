5e44a7be6005bd71155afe84442ab8ff4726f21f
Zero <1300326388@qq.com>
2023-10-27 23:53:28 +0800

    更新路径

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,7 +2,7 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
+  root: 'docs',
   base: '/site/',
   title: 'FlutterAds',
   description: '让你的 Flutter 应用广告收益翻倍',
