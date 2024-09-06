90a47e12a85543c7ec8cca8ddcf518766486a8e6
Zero <1300326388@qq.com>
2024-09-06 10:11:57 +0800

    🚀 适配 github 部署

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: 'docs',
+  base: '/site/',
   title: 'FlutterAds-致力于构建优质的 Flutter 广告插件',
   description: 'FlutterAds 致力于构建优质的 Flutter 广告插件，旨在为 Flutter 开发者提供一套完整的广告解决方案，帮助开发者快速对接广告变现，提升应用收益。主要产品有【Flutter 穿山甲广告、Flutter 短剧小视频、Flutter 广告监测】',
   icon: "/icon.png",
